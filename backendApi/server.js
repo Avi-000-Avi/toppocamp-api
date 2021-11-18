cb2173b14b7a89a00b7a7c2512760a300a68f7dc
Avinash Toppo <atoppo267@gmail.com>
2021-11-18 20:29:40 +0530

    Created Bootcamp Model

--- backendApi/server.js
+++ backendApi/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
+
+
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
-dotenv.config({path:'./config/config.env'});
 
 const app = express();
 
@@ -19,6 +27,13 @@ app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in  ${process.env.NODE_ENV} on PORT ${PORT}`));
+    console.log(`Server running in  ${process.env.NODE_ENV} on PORT ${PORT}`.yellow.bold));
+
+//Handle unhandled promise Rejections
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit processs
+    server.close(() =>  process.exit(1));
+})
