d18a3222e5d2ffdc7f0b148a9acf74a360aae3c5
Avinash Toppo <atoppo267@gmail.com>
2021-11-22 19:04:32 +0530

    GeoCoder Middleware

--- backendApi/middleware/async.js
+++ backendApi/middleware/async.js
@@ -7,3 +7,4 @@ module.exports = asyncHandler;
 
 //The asyncHandler receives a function and returns a function with three input params (oh wait!!! that's like a middleware function). 
 //This new function is responsible to executes the original function passing the three params and catching any error.
+//Replacement for multiple try and catch blocks 
